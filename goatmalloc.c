028ebc570013e12c649b9b276e806fb5a3c879d8
Matthew McAlarney <67719286+MacCode7110@users.noreply.github.com>
2022-02-09 01:05:04 -0500

    Update goatmalloc.c

--- goatmalloc.c
+++ goatmalloc.c
@@ -1,3 +1,4 @@
+
 /*
  ============================================================================
  Name    	: goatmalloc.c
@@ -17,24 +18,44 @@
 #include <sys/mman.h>
 
 void * _arena_start; //_arena_start is of type void * because mmap() returns a pointer to the dynamically mapped region in memory
-int product;
+int result;
 
 int init(size_t requestedsize)
 {
 	//Initialize variables needed to map a virtual address space (allocate memory) for the arena
 	int pagesize = getpagesize();
-	product = pagesize * requestedsize;
+	int protoresult = (int) (requestedsize / pagesize);
+	result = (protoresult * pagesize) + pagesize;
 	int fd = open("/dev/zero", O_RDWR); //open function creates and returns a new file descriptor for the file located at "/dev/zero".
 
+	if(fd == -1)
+	{
+		perror("...failed in initialization, open");
+		return ERR_SYSCALL_FAILED;
+	}
 	//If the byte size requested by the user is less than 0, then return ERR_BAD_ARGUMENTS. Otherwise, map a virtual address space for the arena.
-	if(requestedsize < 0)
+	else if(((int) requestedsize) < 0)
 	{
     	return ERR_BAD_ARGUMENTS;
 	}
 	else
 	{
-    	_arena_start = mmap(NULL, product, (PROT_READ | PROT_WRITE), MAP_PRIVATE, fd, 0);
-    	return (pagesize * requestedsize);
+    	_arena_start = mmap(NULL, result, (PROT_READ | PROT_WRITE), MAP_PRIVATE, fd, 0);
+
+    	if(_arena_start == MAP_FAILED)
+    	{
+    		perror("...failed in initialization, mmap");
+    		return ERR_SYSCALL_FAILED;
+    	}
+    	else if(requestedsize == pagesize)
+    	{
+    		result = pagesize;
+    		return result;
+    	}
+    	else
+    	{
+    		return result;
+    	}
 	}
 
 	//Initialize the chunk list:
@@ -43,15 +64,18 @@ int init(size_t requestedsize)
 
 int destroy()
 {
-	if(_arena_start != NULL)
+	int destroyoutcome = munmap(_arena_start, result);
+
+	if(destroyoutcome == 0)
 	{
-		int result = munmap(_arena_start, product);
-		return result;
+		return destroyoutcome;
 	}
 	else
 	{
+		printf("...error: cannot destroy uninitialized arena. Setting error status\n");
 		return ERR_UNINITIALIZED;
 	}
 }
+
 
 
